3a8b867695955fbf3f55645ef659651b59792eda
Daniel Forigua <forigua.daniel@gmail.com>
2026-05-24 18:31:40 -0500

    Se agregan servicios de autenticacion y productos

--- src/app/core/services/producto.service.ts
+++ src/app/core/services/producto.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Producto } from '../../models/producto.model';
+import { MovimientoRequest, MovimientoResponse } from '../../models/movimiento.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5126/productos';
+
+  constructor(private http: HttpClient) {}
+
+  getInventario(): Observable<Producto[]> {
+    return this.http.get<Producto[]>(`${this.apiUrl}/inventario`);
+  }
+
+  registrarMovimiento(movimiento: MovimientoRequest): Observable<MovimientoResponse> {
+    return this.http.post<MovimientoResponse>(`${this.apiUrl}/movimiento`, movimiento);
+  }
 }
